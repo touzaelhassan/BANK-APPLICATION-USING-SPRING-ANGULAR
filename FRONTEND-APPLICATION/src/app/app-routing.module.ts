1fe64e2223b8b54dfb293983c8660448b556feae
touzaelhassan <touzaelhassan@gmail.com>
2023-01-25 20:43:37 +0100

    Adding navbar, customers, accounts components and configuring routes

--- FRONTEND-APPLICATION/src/app/app-routing.module.ts
+++ FRONTEND-APPLICATION/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { CustomersComponent } from './components/customers/customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "customers", component: CustomersComponent},
+  {path: "accounts", component: AccountsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- FRONTEND-APPLICATION/src/app/app.module.ts
+++ FRONTEND-APPLICATION/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CustomersComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
